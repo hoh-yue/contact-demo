418d4819b03764a91e23bacdf77a27b4132869ed
HUUHUGAA\PC <hohyue@gmail.com>
2021-12-05 12:15:52 +0800

    Add react navigation and dummy screen.

---
+++ src/views/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const Contact = () => {
+  return (
+    <SafeAreaView>
+      <Text>Contact</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Contact;
